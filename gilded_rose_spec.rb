7d1517cbec172828ca7334659c73dc6098759033
Willoughby Hood <willoughbyhood29@googlemail.com>
2021-07-08 17:01:28 +0100

    sell_in date test and qnegative quality test

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -3,15 +3,24 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+    before do
+      @items = [Item.new("foo", 10, 10), Item.new("bar", 10, 0)]
+      GildedRose.new(@items).update_quality()
     end
+    it "does not change the name" do
+      expect(@items[0].name).to eq "foo"
+    end
+
     it "decreases quality of normal item" do
-      items = [Item.new("foo", 10, 10)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 9
+      expect(@items[0].quality).to eq 9
+    end
+
+    it "sell_in decreases" do
+      expect(@items[0].sell_in).to eq 9
+    end
+
+    it "quality never goes below zero" do
+      expect(@items[1].quality).not_to eq -1
     end
   end
 
